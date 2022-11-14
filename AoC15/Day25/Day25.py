3f99129450105070c4c16e219f764f1f6b4a3841
Zeydel <Seydel14>
2022-11-14 13:54:13 +0100

    2015 complete

--- AoC15/Day25/Day25.py
+++ AoC15/Day25/Day25.py
@@ -1,12 +1,25 @@
+# Magic to caculate the index
 def getIndex(row, column):
+    # Return the n'th triangle number
     t = (column*(column+1))/2
-    print(t)
-    t2 = ((row*(row-1))/2)
-    print(t2)
-    return t+(t2*(column-1))
+    # Adds the column number row-1 times
+    t2 = (column*(row-1))
+    # Adds another triangle number
+    t3 = max(0, ((row-1)*(row-2))/2)
+    return int(t+t2+t3)
 
+# Function to quickly get the value, given an index
+def getValue(index):
+    return 20151125*pow(252533,index-1,33554393)%33554393
+
+# Open input and read as words
 f = open('input.txt', 'r')
 presents = f.read().split(' ')
 
+# Extract the row and column
 row = int(presents[16][:-1])
 column = int(presents[18][:-1])
+
+# Find the index and find the results
+index = getIndex(row, column)
+print(f'The value at row {row} and column {column} is {getValue(index)}')
