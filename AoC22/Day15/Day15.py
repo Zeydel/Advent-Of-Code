5407a660adeb516beca0f5e0b40a2f32ae6f3e7c
Zeydel <Seydel14>
2022-12-15 22:40:50 +0100

    Finished day 15

--- AoC22/Day15/Day15.py
+++ AoC22/Day15/Day15.py
@@ -1,3 +1,4 @@
+# Parse the data into dicts of sensors and their nearest beacon
 def parse(data):
     
     sensors = dict()
@@ -16,60 +17,91 @@ def parse(data):
         
     return sensors
 
+# Manhatten distance between two points
 def getDistance(p1, p2):
     
     return abs(p1[0]-p2[0]) + abs(p1[1]-p2[1])
 
-def getBounds(sensors):
+# For a single row, get the set of points where there cannot be a beacon
+def getExclusionsRow(sensors, y):
     
-    xMin, xMax = float('inf'), float('-inf')
-    yMin, yMax = float('inf'), float('-inf')
+    exclusions = set()
     
+    # For every sensor
     for s in sensors:
+        sy = abs(s[1]-y)
         d = getDistance(s, sensors[s])
-        
-        if s[0] + d > xMax:
-            xMax = s[0] + d
-        if s[0] - d < xMin:
-            xMin = s[0] - d
-        if s[1] + d > yMax:
-            yMax = s[1] + d
-        if s[1] - d < yMin:
-            yMin = s[1] - d
+            
+        # Compute the set of x coordinates where there cannot be a beacon
+        exclusions |= set([x for x in range((s[0]-d)+abs(sy), (s[0]+d+1)-abs(sy))])
     
-    return (xMin, xMax, yMin, yMax)
-
-def canBeBeacon(x, y):
-    
-    for s in sensors:
-        
-        if sensors[s] == (x,y):
-            return True
-        
-        if getDistance(s, sensors[s]) >= getDistance(s, (x, y)):
-            return False
-        
-    return True
-
-def getBeaconPos(maxC):
-    
-    for x in range(maxC):
-        for y in range(maxC): 
-    
+    return exclusions
 
 # Open input and read as strings
 f = open('input.txt', 'r')
 data = f.read().split('\n')
 
+# Parse the data
 sensors = parse(data)
-xMin, xMax, yMin, yMax = getBounds(sensors)
 
-notBeaconCounter = 0
-y = 2000000
+# Find all the points where there cannot be a sensor
+y = 10
+exclusions = getExclusionsRow(sensors, y)
 
-for x in range(xMin, xMax+1):
+# Remove all the points that contain a sensor
+for s in sensors:
     
-    if not canBeBeacon(x, y):
-        notBeaconCounter += 1
+    if sensors[s][1] == y and sensors[s][0] in exclusions:
+        exclusions.remove(sensors[s][0])
+
+# Save the length
+exclusionsRow = len(exclusions)
+
+# Problem variables
+lim = 4000000
+frequency = -1
+x, y = 0, 0
+found = False
+
+# Create a dict of closest distances to each sensor
+closestDists = dict()
+for s in sensors:
+    closestDists[s] = getDistance(s, sensors[s])
+
+# Sort the keys by descending x value
+sensors = sorted(sensors, key=lambda se: -se[0])
+
+# While frequency is not found
+while True:
+    
+    # Assume we have found a spot
+    found = True
+    
+    # For every sensor
+    for s in sensors:
         
-print(notBeaconCounter)
+        # If there cannot be a sensor
+        if getDistance(s, (x, y)) <= closestDists[s]:
+            found = False
+            
+            # Move x to outside the signals range and break
+            x = s[0]+(closestDists[s]-abs(s[1]-y))
+            break
+    
+    # If we have found a result, compute the frequency and break
+    if found:
+        frequency = x*4000000 + y
+        break
+            
+    # If x is inside the limit, increment
+    if x <= lim:
+        x += 1
+        
+    # Else reset x and increment y
+    else:
+        x = 0
+        y += 1
+
+# Print the results
+print(f'On row 2000000, {exclusionsRow} points cannot contain a beacon')
+print(f'The frequency is {frequency}')
